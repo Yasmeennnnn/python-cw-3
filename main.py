ee925644522641414a0f43d3ab8b705da206d5ea
YASMEEN <YASMEENSHARRAH@gmail.com>
2023-09-02 15:58:15 +0300

    done

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 favorite_animal =['dog','cat','monkey','rabbit']
 print(favorite_animal)
-print(favorite_animal[1])
-favorite_animal.pop(2)#['dog','cat','rabbit']
+print(f'the second element in the list is {favorite_animal[1]}')
+favorite_animal.remove('monkey')
 print(favorite_animal)
+
 favorite_animal.append('beard')#['dog','cat','monkey','rabbit']
 print(favorite_animal)
-for animal in list:
+for animal in favorite_animal:
+    print (f'i love{animal}')
+
+    numbers=[1,2,3,4,5]
+    numbers_sum=0
+    for number in number :
+        numbers_sum=(numbers_sum+number)
+
+        print(numbers_sum)
