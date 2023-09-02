3eeda22cd4feb2d0b3ff2b0d80c18727b56a76d8
YASMEEN <YASMEENSHARRAH@gmail.com>
2023-09-02 15:57:33 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# write your code here
+favorite_animal =['dog','cat','monkey','rabbit']
+print(favorite_animal)
+print(favorite_animal[1])
+favorite_animal.pop(2)#['dog','cat','rabbit']
+print(favorite_animal)
+favorite_animal.append('beard')#['dog','cat','monkey','rabbit']
+print(favorite_animal)
+for animal in list:
